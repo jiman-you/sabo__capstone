18dbed99eb2bb574b9621c8711a86804961115a9
jiman <dingi0i0@naver.com>
2022-05-13 19:24:08 +0900

    시작페이지

---
+++ SmartLocker/app/src/main/java/com/hansung/android/smartlocker/StartActivity.java
@@ -0,0 +1,26 @@
+package com.hansung.android.smartlocker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        Button startbtn = findViewById(R.id.startbtn);
+        startbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);finish();
+            }
+        });
+    }
+}
